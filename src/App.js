83d37c3d6266a71a7a65ddaed79ea6512462379d
Digamber Singh <digamber@positronx.io>
2020-01-16 22:24:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import addDays from 'date-fns/addDays'
+import "react-datepicker/dist/react-datepicker.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { registerLocale } from  "react-datepicker";
+import es from 'date-fns/locale/es';
+registerLocale('es', es)
+
+class App extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      startDate: new Date()
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    })
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.startDate)
+  }
+ 
+  render() {
+    return (
+      <form onSubmit={ this.onFormSubmit }>
+        <div className="form-group">
+          <DatePicker
+              selected={ this.state.startDate }
+              onChange={ this.handleChange }
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={20}
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy h:mm aa"
+              locale="es"
+              minDate={new Date()}
+              maxDate={addDays(new Date(), 7)}
+          />
+          <button className="btn btn-primary">Show Date</button>
+        </div>
+      </form>
+    );
+  }
+  
 }
 
 export default App;
